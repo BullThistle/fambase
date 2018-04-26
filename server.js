35f0709634f03704439c4b6723271049b4cc5af2
Rafa <rfurry@gmail.com>
2018-04-25 21:36:26 -0700

    Have server.js connect to mongo

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello'));
 
