92b36ffcf4d2444670dfa9866ec1e61656e4faa5
Rafa <rfurry@gmail.com>
2018-04-26 02:50:17 -0700

    Create register validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,14 +5,23 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 
+const validateRegisterInput = require('../../validation/register');
+
 const User = require('../../models/User');
 
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', //size
---
+++ validation/register.js
@@ -0,0 +1,35 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+  dataFields = ['name', 'email', 'password', 'password2'];
+  dataFields.forEach(field => {
+    data[field] = !isEmpty(data[field]) ? data[field] : '';
+    if (Validator.isEmpty(data[field])) {
+      errors[field] = `${field} field is required`;
+    }
+  });
+
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = 'Name must be between 2 and 30 characters';
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = 'Password must be at least 6 characters';
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = 'Confirm Password field is required';
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must match';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
