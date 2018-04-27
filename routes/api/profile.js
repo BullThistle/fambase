03f20969cb6052f84e2a59826d089fdc8c98ddc4
Rafa <rfurry@gmail.com>
2018-04-26 19:34:33 -0700

    Create profile/all route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -28,6 +28,21 @@ router.get(
   }
 );
 
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+});
+
 router.get('/handle/:handle', (req, res) => {
   const errors = {};
 
