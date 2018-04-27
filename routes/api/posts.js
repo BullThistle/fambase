991d8170a3e3fcab006b0c17870aa2a6e15076d1
Rafa <rfurry@gmail.com>
2018-04-26 21:21:28 -0700

    Fix typo in posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,16 +5,28 @@ const passport = require('passport');
 
 const Post = require('../../models/Post');
 
+const validatePostInput = require('../../validation/post');
+
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
-router.post('/', passport('jwt', { session: false }), (req, res) => {
-  const newPost = new Post({
-    test: req.body.test,
-    name: req.body.name,
-    avatar: req.body.avatar,
-    user: req.user.id
-  });
-  newPost.save().then(post => res.json(post));
-});
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(error);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
