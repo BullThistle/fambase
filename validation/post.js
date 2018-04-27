1d87ab8c88e84b45b1ff8c61fb844d64a7c698b8
Rafa <rfurry@gmail.com>
2018-04-26 22:54:44 -0700

    Fix typos in Post model and post validation

--- validation/post.js
+++ validation/post.js
@@ -4,7 +4,7 @@ const isEmpty = require('./is-empty');
 module.exports = function validatePostInput(data) {
   let errors = {};
 
-  data.test = !isEmpty(data.text) ? data.text : '';
+  data.text = !isEmpty(data.text) ? data.text : '';
 
   if (!Validator.isLength(data.text, { min: 2, max: 300 })) {
     errors.text = 'Post must be between 2 and 300 characters';
