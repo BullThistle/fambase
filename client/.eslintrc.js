5605676a91fcc2608537d081461a2d95149e3f02
Rafa <rfurry@gmail.com>
2018-04-27 00:37:55 -0700

    Update .eslintrc to allow windows  linebreaks

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-  extends: "airbnb",
-  parser: "babel-eslint",
+  extends: 'airbnb',
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 6
   },
   rules: {
-    "react/jsx-filename-extension": 0,
-    "import/extensions": "off",
-    "import/no-unresolved": "off"
+    'react/jsx-filename-extension': 0,
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows']
   },
   env: {
     browser: true,
