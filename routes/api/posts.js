8cb866ff2e725e059d645c13c46a1a9fc62b898d
Rafa <rfurry@gmail.com>
2018-04-26 21:49:58 -0700

    Create get /posts and get /post/:id routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,6 +9,21 @@ const validatePostInput = require('../../validation/post');
 
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: 'No post found with that id' })
+    );
+});
+
 router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
