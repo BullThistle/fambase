b6b7393229cb1c099046091ba16548dc79633774
Rafa <rfurry@gmail.com>
2018-04-27 21:40:03 -0700

    Create store.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'styling/semantic.less';
 import { About, Landing } from 'components';
 
 const App = () => (
   <div>
-    <BrowserRouter>
+    <Router>
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/about" component={About} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   </div>
 );
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { AppContainer } from 'react-hot-loader';
+import store from './store';
 import App from './App';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// import reducers from './reducers';
 
 const render = (Component) => {
   ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
       <AppContainer>
         <Component />
       </AppContainer>
@@ -21,5 +19,7 @@ const render = (Component) => {
 
 render(App);
 if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept('./App', () => { render(App); });
+  module.hot.accept('./App', () => {
+    render(App);
+  });
 }
