c858fef02797bff820583e1f073457a00030a953
Rafa <rfurry@gmail.com>
2018-04-28 20:37:11 -0700

    Create Spinner component

---
+++ client/src/components/Common/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+const IconExampleLoading = () => (
+  <div>
+    <Icon loading size="big" name="spinner" />
+  </div>
+);
+
+export default IconExampleLoading;
