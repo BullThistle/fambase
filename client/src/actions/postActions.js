05e8938a731e8c0e4d8337a52621328ccb4c6cbb
Rafa <rfurry@gmail.com>
2018-05-02 23:12:45 -0700

    Create getPost and deletePost actions

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,7 +2,17 @@
 /* eslint-disable function-paren-newline */
 import axios from 'axios';
 
-import { ADD_POST, GET_ERRORS } from './types';
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING,
+  DELTETE_POST,
+} from './types';
+
+export const setPostLoading = () => ({
+  type: POST_LOADING,
+});
 
 export const addPost = postData => (dispatch) => {
   axios
@@ -11,6 +21,41 @@ export const addPost = postData => (dispatch) => {
       dispatch({
         type: ADD_POST,
         payload: res.data,
+      }),
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      }),
+    );
+};
+
+export const getPosts = () => (dispatch) => {
+  dispatch(setPostLoading());
+  axios
+    .get('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data,
+      }),
+    )
+    .catch(() =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null,
+      }),
+    );
+};
+
+export const deletePost = id => (dispatch) => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(() =>
+      dispatch({
+        type: DELTETE_POST,
+        payload: id,
       }),
     )
     .catch(err =>
